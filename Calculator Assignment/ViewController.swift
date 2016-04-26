80f1c75fc11b5ce5cc5421bbaa40dc1c57c413e1
Mark Rabins <markrabins@gmail.com>
2016-04-25 23:14:49 -0700

    Additional UI Buttons, display label set up

--- Calculator Assignment/ViewController.swift
+++ Calculator Assignment/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        displayLabel.textAlignment = NSTextAlignment.Right
+        displayLabel.text = "0"
+        displayLabel.backgroundColor = UIColor(red: 153/255, green: 255/255, blue: 153/255, alpha: 1.0)
         
         
     }
