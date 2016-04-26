a5cff631202bb6ed8bcfeffa77bf7e9f4fd1d7c9
Mark Rabins <markrabins@gmail.com>
2016-04-25 23:31:25 -0700

    All button shell functions

--- Calculator Assignment/ViewController.swift
+++ Calculator Assignment/ViewController.swift
@@ -27,6 +27,118 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Operation IBActions
+    
+    @IBAction func multiplicationTapped(sender: UIButton) {
+    
+    }
+    
+    @IBAction func divideTapped(sender: UIButton) {
+    
+    }
+    
+    @IBAction func subtractTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func plusTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func equalsTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func plusMinusTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func backspaceTapped(sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    // Symbol IBActions
+    
+    @IBAction func decimalTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func zeroTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func oneTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func twoTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func threeTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func fourTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func fiveTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func sixTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func sevenTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func eightTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func nineTapped(sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 }
